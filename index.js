4aeb140ec63c7228bf43abdb0d4232bc39bf411b
Fabrizio Zagone <fabriziozagone03@gmail.com>
2023-04-26 10:58:58 +0200

    show id (don't function)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
 
-app.use(express.json()) // for parsing application/json
+app.use(bodyParser.json()) // for parsing application/json
 
 app.get('/', (req, res) => {
     console.log(req.query);
@@ -12,6 +13,11 @@ app.get('/', (req, res) => {
     let sum = parseInt(num1) + parseInt(num2);
     console.log(sum);
     res.send(200,sum);
+});
+
+app.get('/users/:id', (req, res) => {
+    const userID = req.params.id;
+    res.send("Hello, user #" + userID);
 });
 
 app.post('/', function (req, res) {
